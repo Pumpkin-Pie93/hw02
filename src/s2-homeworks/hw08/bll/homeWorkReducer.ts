ac7eee50dbf3f3436066e602c873301a3acda24a
Polina Khmialnitskaya <polinagreenheart@gmail.com>
2023-12-26 17:03:49 +0300

    hw8

--- src/s2-homeworks/hw08/bll/homeWorkReducer.ts
+++ src/s2-homeworks/hw08/bll/homeWorkReducer.ts
@@ -1,20 +1,38 @@
 import {UserType} from '../HW8'
 
+const initialState:UserType[] = [
+    {_id: 0, name: 'Кот', age: 13},
+    {_id: 1, name: 'Александр', age: 66},
+    {_id: 2, name: 'Коля', age: 16},
+    {_id: 3, name: 'Виктор', age: 44},
+    {_id: 4, name: 'Дмитрий', age: 40},
+    {_id: 5, name: 'Ирина', age: 55},
+]
+
+
 type ActionType =
     | { type: 'sort'; payload: 'up' | 'down' }
     | { type: 'check'; payload: number }
 
-export const homeWorkReducer = (state: any, action: any): any => { // need to fix any
+export const homeWorkReducer = (state = initialState, action: ActionType): any => { // need to fix any
     switch (action.type) {
         case 'sort': { // by name
+            let newState = [...state];
+            if (action.payload === 'up') {
+            newState.sort((a, b) =>( a.name.localeCompare(b.name)))
+            }  if (action.payload === 'down') {
+            newState.sort((a, b) => (b.name.localeCompare(a.name)))
+            }
+               return newState
 
-            return state // need to fix
         }
         case 'check': {
 
-            return state // need to fix
+            return state.filter(user => user.age >= 18) // need to fix
         }
         default:
             return state
     }
 }
+
+
